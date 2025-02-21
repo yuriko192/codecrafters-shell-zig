7be6258b62eb3943c6e3c448b1f530008a02b4fa
yuriko192 <ivanwijaya19@yahoo.co.id>
2025-02-21 11:19:44 +0700

    add repl

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,14 @@ const std = @import("std");
 pub fn main() !void {
     // Uncomment this block to pass the first stage
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
-
     const stdin = std.io.getStdIn().reader();
     var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    try stdout.print("{s}: command not found\n", .{user_input});
+    while (true) {
+        try stdout.print("$ ", .{});
 
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        try stdout.print("{s}: command not found\n", .{user_input});
+    }
 }
